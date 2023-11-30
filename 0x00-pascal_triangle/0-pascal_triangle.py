8975426961d80126fa70f25a3190bc672968cf3d
Senyo Fiawornu <senyo332@gmail.com>
2023-11-30 22:14:20 +0000

    initial commit

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+'''Pascal's triangle.
+'''
+
+def pascal_triangle(n):
+    '''returns a list of lists of integers representing
+       the Pascal’s triangle of n
+    '''
+    triangle = []
+    if type(n) is not int or n <= 0:
+        return triangle
+    
+    i = 0
+    while i < n:
+        line = []
+        j = 0
+        while j < (i + 1):
+            if j == 0 or j == i:
+                line.append(1)
+            elif i > 0 and j > 0:
+                line.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+            j += 1
+        triangle.append(line)
+        i += 1
+    
+    return triangle
