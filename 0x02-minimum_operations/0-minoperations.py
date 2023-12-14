46fb52661aa2de9cdda300850794a713955cbf37
Senyo Fiawornu <contactsenyo@gmail.com>
2023-12-14 15:27:05 +0000

    passed all lint tests

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+'''calculates the fewest number of operations needed
+to result in exactly n H characters in the file.
+'''
+
+
+def min_operations(n):
+    '''Returns an integer.
+    If n is imposible to achieve, return 0
+    '''
+    if n <= 1:
+        return 0
+
+    factors = []
+    d = 2
+    while d * d <= n:
+        while (n % d) == 0:
+            factors.append(d)
+            n //= d
+        d += 1
+
+    if n > 1:
+        factors.append(n)
+
+    result = 0
+    for factor in factors:
+        result += factor
+
+    return result
