efe17e0d24086d0c6987f64c9f4f6c2316141b4a
root <root@DESKTOP-JKHB9QO>
2024-01-31 22:23:17 +0000

    task 0 completed

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -5,24 +5,31 @@
 
 
 def makeChange(coins, total):
-    """Determines the fewest number of coins needed to
-    meet a given amount total.
-    """
-
+    '''
+       determines the fewest number of coins needed
+       to meet a given amount total
+    '''
     if total <= 0:
         return 0
 
+    '''
+    Initialize an array to store the minimum number of coins
+    needed for each total
+    '''
     dp = [float('inf')] * (total + 1)
-    dp[0] = 0  # Base case
 
-    """Optimizations:"""
-    coins.sort()  # Sort coins in ascending order for early termination
+    # Base case: 0 coins needed to make a total of 0
+    dp[0] = 0
 
+    '''
+    Iterate through each coin and update the minimum number
+    of coins needed for each total
+    '''
     for coin in coins:
         for i in range(coin, total + 1):
-            """Early termination if coin is too large"""
-            if coin > i:
-                break
             dp[i] = min(dp[i], dp[i - coin] + 1)
 
+    '''If the final value at the total is still infinity,
+    it means the total cannot be met
+    '''
     return dp[total] if dp[total] != float('inf') else -1
