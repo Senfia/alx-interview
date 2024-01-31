62e5b9c2d2e9fc7b3a6f8a2c4981cbbbb3fa0ac9
root <root@DESKTOP-JKHB9QO>
2024-01-31 22:18:39 +0000

    optimised

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -5,20 +5,24 @@
 
 
 def makeChange(coins, total):
-    '''
-       determines the fewest number of coins needed
-       to meet a given amount total
-    '''
+    """Determines the fewest number of coins needed to
+    meet a given amount total.
+    """
+
     if total <= 0:
         return 0
 
     dp = [float('inf')] * (total + 1)
+    dp[0] = 0  # Base case
 
-    # Base case: 0 coins needed to make a total of 0
-    dp[0] = 0
+    """Optimizations:"""
+    coins.sort()  # Sort coins in ascending order for early termination
 
     for coin in coins:
         for i in range(coin, total + 1):
+            """Early termination if coin is too large"""
+            if coin > i:
+                break
             dp[i] = min(dp[i], dp[i - coin] + 1)
 
     return dp[total] if dp[total] != float('inf') else -1
